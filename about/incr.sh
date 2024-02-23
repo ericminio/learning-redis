37b856714431ba9d8afd2f9ec286e879098f3df6
ericminio <eric.mignot@gmail.com>
2024-02-22 23:58:23 -0800

    use docker exec

--- about/incr.sh
+++ about/incr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function test_incr_increments_by_1 {
-    docker-compose run --rm redis redis-cli -h redis-6 SET counter 0
-    docker-compose run --rm redis redis-cli -h redis-6 INCR counter > output
-    value=$(cat output | grep "(integer) 1" | wc -l)
+    docker exec redis-6 redis-cli -h redis-6 SET counter 0
+    docker exec redis-6 redis-cli -h redis-6 INCR counter > output
+    value=$(cat output)
 
-    assertequals $value 1
+    assertequals "$value" "1"
 }
--- support/waiting.sh
+++ support/waiting.sh
@@ -4,7 +4,7 @@ function waiting {
     ready=0
     while [ "$ready" -eq "0" ]
     do    
-        ready=`docker-compose run --rm redis redis-cli -h redis-6 INFO server | grep "redis_version" | wc -l`
+        ready=`docker exec redis-6 redis-cli -h redis-6 INFO server | grep "redis_version" | wc -l`
         if [ "$ready" -eq "0" ]; then    
             echo "Waiting for redis instance redis-6"
             sleep 1;
