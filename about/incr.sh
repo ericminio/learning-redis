d4169ea79a19b20b62093ff092b1cd692d0bab19
ericminio <eric.mignot@gmail.com>
2024-02-23 00:17:56 -0800

    incr

--- about/incr.sh
+++ about/incr.sh
@@ -4,8 +4,8 @@ source ./support/cli.sh
 
 function test_incr_increments_by_1 {
     redis-cli SET counter 0
-    redis-cli INCR counter > output
-    value=$(cat output)
+    redis-cli INCR counter
+    value=$(redis-cli GET counter)
 
     assertequals "$value" "1"
 }
